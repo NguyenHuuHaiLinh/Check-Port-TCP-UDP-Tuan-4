b81c7bc44381418704f83222cb6ec0595c820146
NguyenHuuHaiLinh <nahaka9901@gmail.com>
2018-04-03 18:00:26 +0700

    Tuan4

---
+++ CheckPortTCP/CheckPortTCP/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace CheckPortTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("nhap port: ");
+            n = int.Parse(Console.ReadLine());
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("172.18.2.103"), n);
+            using (TcpClient tcpClient = new TcpClient())
+            {
+                try
+                {
+                    tcpClient.Connect(ipe);
+                    Console.WriteLine("TCP Port Open ");
+                    tcpClient.Close();
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("TCP Port Close");
+                }
+            }
+            //udp
+           
+            UdpClient udp = new UdpClient();
+            udp.Connect(ipe);
+            byte[] data = Encoding.ASCII.GetBytes("aaaaaaaa");
+            udp.Send(data, data.Length);
+            try
+            {
+                udp.Receive(ref ipe);
+                Console.WriteLine("port udp mở");
+                udp.Close();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("port udp đóng");
+            }
+            Console.ReadLine();
+        }
+    }
+}
